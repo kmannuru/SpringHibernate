e04f83fa240c7d9015b0abc7e1e6290150c5f582
reddy kishore mannuru <kishore@reddys-iMac.local>
2017-11-10 06:54:10 -0800

    Initial Commit

---
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach{
+	@Override
+	public String getDailyWorkout() {
+		return "practice batting in cage for 1 hour";
+	}
+
+}
---
+++ src/com/luv2code/springdemo/MainApp.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Coach coach = new TrackCoach();
+		System.out.println(coach.getDailyWorkout());
+
+	}
+
+}
